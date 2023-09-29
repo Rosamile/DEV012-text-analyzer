c17a3621508ba5c932e5d0c4f6e2adbdeb976a35
Rosa Sarmiento <rosamilets@gmail.com>
2023-09-29 11:36:59 -0500

    29/0923

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+import { types } from '@babel/core';
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+//let userInput = "texTarea";
+//console.log ("textTarea")
+
+//let wordCount = wordCount > 0 ;
+
+//function limpiar () {console.log (borrar)}
+//$resetButton.onclick = "limpiar ()";
+
+//formula evento semantico 
+
+(function limpiar (){
+    let userInput = document.getElementById("user-input");
+        resetButton.onclick("click", function () {
+        userInput.value = "";
+        })});
+
+//let characterCount ="userInput"
+//let characterNoSpaces = "userInput"
+//let numberCount = "userInput"
+//let numberSum = "userInput"
+//let wordLengthAverage = "userInput"   
+
